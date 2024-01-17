d721c7953e53f7f6c790b6a1bb866e99b0909786
aob93 <60762425+aob93@users.noreply.github.com>
2024-01-17 15:30:33 +0000

    reformat_GTEx_phenotypes.sh

---
+++ reformat_GTEx_phenotypes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# the following script reformats GTEx phenotype BED files and reindexes them
+
+# Directory containing your .bed.gz files
+DIRECTORY="/path/to/your/directory"
+
+# Loop over each .bed.gz file in the directory
+for file in $DIRECTORY/*.bed.gz
+do
+    # Remove 'chr' prefix and create a new intermediate file
+    zcat "$file" | awk '{ sub(/^chr/, "", $1); print }' | gzip > "${file%.bed.gz}.mod.bed.gz"
+
+    # Sort the modified file according to BED format
+    zcat "${file%.bed.gz}.mod.bed.gz" | sort -k1,1 -k2,2n | gzip > "${file%.bed.gz}.sorted.mod.bed.gz"
+
+    # Remove intermediate unsorted file
+    rm "${file%.bed.gz}.mod.bed.gz"
+
+    # Index the sorted file with tabix
+    tabix -p bed "${file%.bed.gz}.sorted.mod.bed.gz"
+done
